567dd1f61309461e69e8cad92ab190212aa738ea
kaizsv <kai.zsv@gmail.com>
2021-06-16 13:52:18 +0800

    First commit

---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+UNAME=$(uname -s)
+
+if [ $UNAME == "Linux" ]; then
+
+    PACS="vim git valgrind htop curl cloc tree tmux wget net-tools cscope stow
+        cmake flex bison strace qemu tcl tk minicom autoconf vlc bash-completion
+        nmap make trace-cmd okular evince graphviz gnuplot gcc thunderbird gdb
+        audacious gnome-tweak-tool pcsc-tools emacs libreoffice"
+    DEBS="software-properties-common python3-pip exuberant-ctags python3-dev
+        apt-transport-https autotools-dev qemu-utils libvirt-clients pcscd
+        libvirt-daemon-system clang-format haskell-platform texlive-full
+        build-essential gcc-arm-none-eabi gdb-multiarch"
+
+    if [[ "$(lsb_release -d)" == *"Ubuntu"* ]]; then
+        sudo apt update
+        sudo apt install -y $PACS $DEBS linux-tools-common linux-tools-generic
+    elif [[ "$(lsb_release -d)" == *"Debian"* ]]; then
+        sudo apt update
+        sudo apt install -y $PACS $DEBS linux-tools linux-perf
+    elif [ -f "/etc/arch-release" ]; then
+        sudo pacman -Syu $PACS ctags libvirt clang texlive-most atom
+    fi
+
+    unset PACS
+    unset DEBS
+
+    # vim-plug
+    [ ! -f "$HOME/.vim/autoload/plug.vim" ] && \
+        curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+    mkdir -p $HOME/usr
+    mkdir -p $HOME/tmp
+    [ -d "$HOME/Templates" ] && touch ~/Templates/UntitiledDocument
+
+    # cron job: * * * * * export DISPLAY=:0; /usr/bin/quiterss
+    [ ! -d "$HOME/usr/quiterss" ] && \
+        git clone https://github.com/QuiteRSS/quiterss.git ~/usr/quiterss
+    [ ! -d "$HOME/usr/goldendict" ] && \
+        git clone https://github.com/goldendict/goldendict.git ~/usr/goldendict
+
+    # install dotfiles
+    if [ -x "$(command -v stow)" ]; then
+        for target in "gdb git vim tmux bash"
+        do
+            stow --dotfiles $target -t $HOME
+        done
+    else
+        echo "Install stow"
+    fi
+
+fi
+
+unset UNAME
